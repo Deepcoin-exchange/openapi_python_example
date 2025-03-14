5d80646439c6b4a95e617f9d2bd4ef894dc199db
zhaozonghao <zhaozonghao@deepcoin.info>
2025-03-15 06:52:57 +0800

    update example as "ws"

--- utils/request_util.py
+++ utils/request_util.py
@@ -27,7 +27,7 @@ class BaseMethod(object):
         uri_append = Util().get_uri(uri=self.uri, data=data, method=self.method)
         print(f"uri:{uri_append}")
         sign = Util().encode(key=self.serect_key, iso_time=self.iso_time, method=self.method, data=data, uri=uri_append)
-        self.headers['OK-ACCESS-SIGN'] = sign
+        self.headers['DC-ACCESS-SIGN'] = sign
         url = self.host + uri_append
         resp = Util().send_request(self.method, url, self.headers, data)
         print(f"resp:{resp}")
